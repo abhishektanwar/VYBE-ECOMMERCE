6cb728d6892b23111ec61f2be2272696dff7f448
=abhishektanwar <abhishektanwar084@gmail.com>
2022-03-16 11:10:56 +0530

    hero carousel added

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
+import ImageSlider from "../../components/ImageSlider";
+import { SliderData } from "../../components/ImageSlider/SliderData";
 
 const Home = () => {
   return (
+    <>
     <nav className="nav-bar shadow-box" id="my-nav-bar">
       <Header />
     </nav>
+    <ImageSlider sliderData={SliderData} />
+    </>
   );
 };
 
