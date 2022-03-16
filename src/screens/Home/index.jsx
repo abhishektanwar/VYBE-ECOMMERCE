f861b303a1e97f7e2f4099844e460a47ee5d4a7a
=abhishektanwar <abhishektanwar084@gmail.com>
2022-03-16 14:46:59 +0530

    procomponent added : product card

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import CardSlider from "../../components/CardSlider";
 import Header from "../../components/Header";
 import ImageSlider from "../../components/ImageSlider";
 import { SliderData } from "../../components/ImageSlider/SliderData";
-
+import ProductCard from "../../components/ProductCard";
+import "./home.css";
 const Home = () => {
   return (
     <>
-    <nav className="nav-bar shadow-box" id="my-nav-bar">
-      <Header />
-    </nav>
-    <ImageSlider sliderData={SliderData} />
+      <nav className="nav-bar shadow-box" id="my-nav-bar">
+        <Header />
+      </nav>
+      <ImageSlider sliderData={SliderData} />
+
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <CardSlider slides={[1,2,3,4,5,6,7,8]} />
+      </div>
     </>
   );
 };
