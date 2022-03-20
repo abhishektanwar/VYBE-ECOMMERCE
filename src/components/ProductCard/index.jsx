9023188bfbe412b463d3aeebc13f0d19db19e366
=abhishektanwar <abhishektanwar084@gmail.com>
2022-03-20 18:42:18 +0530

    horizontal product card variant added

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
-import './product-card.css'
+import React from "react";
+import "./product-card.css";
 const ProductCard = (props) => {
-  
-  const {imagePath,badgeText,title,price,actualPrice,discountPercentage} = props;
+  const {
+    imagePath,
+    badgeText,
+    title,
+    price,
+    actualPrice,
+    discountPercentage,
+    variant,
+  } = props;
+  if (variant === "horizontal") {
+    return (
+      <div class="horizontal-card default-container margin-trb-16 shadow-box">
+        <div class="image-container badge-container">
+          <img
+            src={imagePath}
+            loading="lazy"
+            alt="product-image"
+            class="responsive-img image-container"
+          />
+          <span class="badge primary-card-badge">New Arrival</span>
+        </div>
+
+        <div class="card-text-container flex-row">
+          <div className="flex-column">
+            <div>
+              <h6 class="text-light-weight">ONEAR 100 HEADPHONES BLACK</h6>
+              <p class="body-typo-sm">
+                {/* Advance Noise Canceling, Ambient Sound, Listening Optimizer */}
+                {/* <br /> */}
+                <small class="body-typo-xs text-light-weight secondary-text-color">
+                  Referende Id:7837A7
+                </small>
+              </p>
+            </div>
+            <div class="text-container-description">
+              <p class="typo-xs text-bold-weight">
+                {`${price} `}
+                {actualPrice && discountPercentage && (
+                  <span class="text-light-weight">
+                    <span class="typo-strike-through body-typo-sm">
+                      Rs.2099
+                    </span>
+                    <span class="primary-text-color"> 10%</span>
+                  </span>
+                )}
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="card-action-btn-container flex-column"
+            style={{ marginLeft: "auto" }}
+          >
+            <button class="margin-trb-16 btn btn-filled-primary">
+              Add to Cart
+            </button>
+            <button class="margin-trb-16 btn btn-outline-primary">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     // <div className='product-card-container'>
     //   <div className="product-card-image">
@@ -10,45 +72,54 @@ const ProductCard = (props) => {
     //   </div>
     // </div>
     <div class="vertical-card flex-column margin-trb-20">
-						<div class="image-container badge-container">
-							<img src={imagePath} loading="lazy" alt="product-image"
-								class="responsive-img image-container" />
-                {badgeText && <span class="badge primary-card-badge text-bold-weight">{badgeText}</span> }
-							
-						</div>
+      <div class="image-container badge-container">
+        <img
+          src={imagePath}
+          loading="lazy"
+          alt="product-image"
+          class="responsive-img image-container"
+        />
+        {badgeText && (
+          <span class="badge primary-card-badge text-bold-weight">
+            {badgeText}
+          </span>
+        )}
+      </div>
 
-						<div class="card-text-container margin-top-20">
-							<div>
-								<div class="flex-row flex-align-item-center">
-									<h6 class="text-medium-weight body-typo-md">
-                    {title}
-									</h6>
-									<button href="#" class="favorite-btn">
-										<span>
-											<i class="far fa-heart"></i>
-										</span>
-									</button>
-								</div>
-							</div>
-							<div class="text-container-description">
-								<p class="typo-xs text-bold-weight">
-									{`${price} `}
-                  {actualPrice && discountPercentage &&
-									<span class="text-light-weight"><span class="typo-strike-through body-typo-sm">Rs.2099</span><span
-											class="primary-text-color"> 10%</span></span>}
-								</p>
-							</div>
+      <div class="card-text-container margin-top-20">
+        <div>
+          <div class="flex-row flex-align-item-center">
+            <h6 class="text-medium-weight body-typo-md">{title}</h6>
+            <button href="#" class="favorite-btn">
+              <span>
+                <i class="far fa-heart"></i>
+              </span>
+            </button>
+          </div>
+        </div>
+        <div class="text-container-description">
+          <p class="typo-xs text-bold-weight">
+            {`${price} `}
+            {actualPrice && discountPercentage && (
+              <span class="text-light-weight">
+                <span class="typo-strike-through body-typo-sm">Rs.2099</span>
+                <span class="primary-text-color"> 10%</span>
+              </span>
+            )}
+          </p>
+        </div>
 
-							<div class="card-action-btn-container">
-								<button class="margin-trb-16 btn btn-filled-primary" style={{width:'100%'}}>
-									Add to Cart
-								</button>
-								
-							</div>
-						</div>
-					</div>
+        <div class="card-action-btn-container">
+          <button
+            class="margin-trb-16 btn btn-filled-primary"
+            style={{ width: "100%" }}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default ProductCard
+export default ProductCard;
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -7,17 +7,14 @@ import ImageSlider from "../../components/ImageSlider";
 import { SliderData } from "../../components/ImageSlider/SliderData";
 import "./home.css";
 import {Link} from 'react-router-dom'
+import ProductCard from "../../components/ProductCard";
 const Home = () => {
   return (
     <>
-      {/* <nav className="nav-bar shadow-box" id="my-nav-bar">
-        <Header />
-      </nav> */}
       <button><Link to="/products" >Products page</Link></button>
       
       <ImageSlider sliderData={SliderData} />
       <FeaturedContainer featuredContainerTitle={'Featured Categories'} />
-
       <div style={{ display: "flex", justifyContent: "space-around" }} className="margin-top-60">
         <CardSlider slides={[1,2,3,4,5,6,7,8]} />
       </div>
