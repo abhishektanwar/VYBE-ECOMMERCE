25e89abe9e005953e37a266142d564a626502537
=abhishektanwar <abhishektanwar084@gmail.com>
2022-04-06 04:58:24 +0530

    explore products button added in home screen

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -5,15 +5,25 @@ import { SliderData } from "../../components/ImageSlider/SliderData";
 import FeaturedContainer from "../../components/FeaturedContainer";
 import { useProductListing } from "../../Contexts/ProductListingContext";
 import { useDocumentTitle } from "../../helpers/helpers";
+import {useNavigate} from 'react-router-dom';
+import Button from "../../components/Header/Button";
 import "./home.css";
 
 const Home = () => {
   const { productListingState } = useProductListing();
   const { categoriesData, products } = productListingState;
+  const navigate = useNavigate();
   useDocumentTitle("Trek Shark | Home");
   return (
     <>
       <ImageSlider sliderData={SliderData} />
+      <div className="flex-row flex-justify-content-center">
+      <Button
+        buttonText="Explore Products"
+        buttonStyle="hero-image-btn"
+        onClick={() => navigate("/products")}
+      />
+      </div>
       <FeaturedContainer
         featuredContainerTitle={"Featured Categories"}
         categories={categoriesData}
