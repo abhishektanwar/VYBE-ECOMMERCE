e334e8ea5f9928c01c8a4ff0270580c618bc8357
=abhishektanwar <abhishektanwar084@gmail.com>
2022-04-06 02:09:58 +0530

    fix auth after refresh

--- src/Contexts/AuthDialogContext/index.jsx
+++ src/Contexts/AuthDialogContext/index.jsx
@@ -1,9 +1,10 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import Login from "../../components/Authentication/Login";
 import SignUp from "../../components/Authentication/SignUp";
 import ModalWrapper from "../../components/ModalWrapper";
 import axios from "axios";
 import utils from "../../utils";
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({
   showModal: false,
@@ -16,7 +17,10 @@ export const AuthProvider = (props) => {
   const [authType, setAuthType] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({ errorExists: false, errorMessage: "" });
-  const [user,setUser] = useState(null);
+  const [user,setUser] = useState({
+    isAuthenticated: false,
+    encodedToken: "",
+  });
 
   const loginHandler = async (user) => {
     setIsLoading(true);
@@ -29,11 +33,17 @@ export const AuthProvider = (props) => {
         utils.setLocalStorage('authToken',res.data.encodedToken);
         setIsLoading(false);
         setError({errorExists:false,errorMessage:''})
-        setUser(res.data.foundUser)
+        setUser({
+          ...res.data.foundUser,
+          isAuthenticated: true,
+          encodedToken: res.data.encodedToken,
+        });
+        toast.success("Logged in successfully")
         console.log("res.data.foundUser",res.data.foundUser)
         return true;
       }
     } catch (e) {
+      toast.success("Log in failed")
       setError({errorExists:true,errorMessage:"Invalid Credentials. Please try again"});
       setIsLoading(false);
     }
@@ -41,6 +51,10 @@ export const AuthProvider = (props) => {
 
   const logoutHandler = () => {
     utils.removeLocalStorage('authToken')
+    setUser({
+      isAuthenticated: false,
+      encodedToken: "",
+    });
 		setUser(null);
   };
   
@@ -52,7 +66,12 @@ export const AuthProvider = (props) => {
         password: user.password,
       })
       if(res.statusText==='Created'){
-        setUser(res.data.createdUser)
+        setUser({
+          ...res.data.foundUser,
+          isAuthenticated: true,
+          encodedToken: res.data.encodedToken,
+        });
+        toast.success("Signed up successfully")
         utils.setLocalStorage('authToken',res.data.encodedToken);
         setError({errorExists:false,errorMessage:''})
         setIsLoading(false)
@@ -60,10 +79,18 @@ export const AuthProvider = (props) => {
       }
     }
     catch(e){
+      toast.success("Sign up failed")
       setError({errorExists:true,errorMessage:"Something went wront. Please try again"});
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    const trekSharkAuthToken = utils.getLocalStorage("authToken");
+    if (trekSharkAuthToken) {
+      setUser({isAuthenticated:true,encodedToken:trekSharkAuthToken})
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user,authType, setAuthType,loginHandler, signUpHandler,logoutHandler,error }}>
